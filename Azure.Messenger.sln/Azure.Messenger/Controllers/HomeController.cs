1d714c747a81326b8d00eeae76952958c9158041
Ishanka Lakshan Wasalage <isankalakshan@gmail.com>
2015-11-24 15:10:18 +0530

    added .ignore file

--- Azure.Messenger.sln/Azure.Messenger/Controllers/HomeController.cs
+++ Azure.Messenger.sln/Azure.Messenger/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Azure.Messenger.Models;
 
 namespace Azure.Messenger.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]  
+        public ActionResult Index(MessageViewModel Message)
+        {
+            return View();
+        }
+
     }
 }
---
+++ Azure.Messenger.sln/Azure.Messenger/Models/MessageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Azure.Messenger.Models
+{
+    public class MessageViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
